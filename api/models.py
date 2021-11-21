62fb9d5d0d08a365a03f97260a5e788aeacba8d0
Sungeheon <elon4856@gmail.com>
2021-11-22 04:48:06 +0900

    feat: 모델링구현.

--- api/models.py
+++ api/models.py
@@ -40,19 +40,19 @@ class User(models.Model):
 
 class UserRecords(models.Model):
     deer = models.ForeignKey("Deer", on_delete=models.DO_NOTHING)
-    end_lat = models.PointField()
-    end_lng = models.PointField()
+    end_lat_lng = models.PointField()
     start_at = models.DateTimeField()
     end_at = models.DateTimeField()
+    user = models.ForeignKey("User", on_delete=models.DO_NOTHING)
 
     class Meta:
         db_table = 'user_records'
-
+        get_latest_by = ["end_at"]
 
 class ParkingZone(models.Model):
-    center_lat = models.PointField()
-    center_lng = models.PointField()
+    center_lat_lng = models.PointField()
     radius = models.FloatField()
+    area = models.ForeignKey("Area", on_delete=models.DO_NOTHING)
 
     class Meta:
         db_table = 'parking_zone'
@@ -61,6 +61,7 @@ class ParkingZone(models.Model):
 class ForbiddenArea(models.Model):
     boundary = models.PolygonField()
     coords = models.MultiPointField()
+    area = models.ForeignKey("Area", on_delete=models.DO_NOTHING)
 
     class Meta:
         db_table = 'forbidden_area'
