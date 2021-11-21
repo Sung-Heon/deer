ff4865a98fba5892a71938e4c019a026f7cf9c99
Sungeheon <elon4856@gmail.com>
2021-11-21 23:12:34 +0900

    add: model 추가 - User - UserRecords - BasePricing - ParkingZone - ForbiddenArea

---
+++ api/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.gis.db import models
+
+
+# Create your models here.
+
+class Area(models.Model):
+    boundary = models.PolygonField()
+    center = models.PointField()
+    coords = models.MultiPointField()
+
+    class Meta:
+        db_table = 'area'
+
+
+class Deer(models.Model):
+    name = models.CharField()
+    area = models.ForeignKey("Area", on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'deer'
+
+
+class BasePricing(models.Model):
+    name = models.CharField()
+
+    class Meta:
+        db_table = 'base_pricing'
+
+
+class User(models.Model):
+    name = models.CharField()
+    base_pricing = models.ForeignKey("BasePricing", on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'user'
+
+
+class UserRecords(models.Model):
+    deer = models.ForeignKey("Deer", on_delete=models.DO_NOTHING)
+    end_lat = models.PointField()
+    end_lng = models.PointField()
+    start_at = models.DateTimeField()
+    end_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'user_records'
+
+
+class ParkingZone(models.Model):
+    center_lat = models.PointField()
+    center_lng = models.PointField()
+    radius = models.FloatField()
+
+    class Meta:
+        db_table = 'parking_zone'
+
+
+class ForbiddenArea(models.Model):
+    boundary = models.PolygonField()
+    coords = models.MultiPointField()
+
+    class Meta:
+        db_table = 'forbidden_area'
