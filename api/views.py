e09ab876fac7bfa5ce0182fc206515b8bc15043b
Sungeheon <elon4856@gmail.com>
2021-11-22 04:48:18 +0900

    feat: api구현.

--- api/views.py
+++ api/views.py
@@ -1,22 +1,33 @@
-from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
-
+from .charging_policies.dto import UserDTO
 import json
-# Create your views here.
+from datetime import datetime
+from api.cost_calculator import CostCalculator
+from .models import  User, Deer
 
 class CalculateChargeView(View):
+
     def post(self, request):
         data = json.loads(request.body)
-        # user = request.user
-        print(data)
-        deer_name = data['deer_name']
-        end_lat = data['end_lat']
-        end_lng = data['end_lng']
-        start_at = data['start_at']
-        end_at = data['end_at']
+
+        userdto = UserDTO()
+        user = User.objects.get(name= data["user_name"])
+        deer =  Deer.objects.get(name=data['deer_name'])
+        userdto.user_id = user.id
+        userdto.use_deer = deer
+        userdto.use_end_lat = data['end_lat']
+        userdto.use_end_lng = data['end_lng']
+        userdto.use_start_at = datetime.strptime(data['start_at'], '%Y-%m-%d %H:%M:%S')
+        userdto.use_end_at = datetime.strptime(data['end_at'], '%Y-%m-%d %H:%M:%S')
+
+
+        costCalculator = CostCalculator(userdto)
+
+        policies_and_charge =costCalculator.calculate()
+
+        return JsonResponse({'message': 'SUCCESS', 'charge_cost':f'{policies_and_charge[-1]}, "요금 근거: f{policies_and_charge[:-1]}'}, status=200)
 
 
 
-        return JsonResponse({'message': 'SUCCESS', "deer_name": f'{deer_name}'}, status=200)
 
