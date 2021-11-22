c634b2a6949b7b991730a956f2a1714fedaa1fa4
Sungeheon <elon4856@gmail.com>
2021-11-22 13:36:32 +0900

    feat: userrecords 기록 기능 추가.

--- api/views.py
+++ api/views.py
@@ -4,7 +4,9 @@ from .charging_policies.dto import UserDTO
 import json
 from datetime import datetime
 from api.cost_calculator import CostCalculator
-from .models import  User, Deer
+from .models import User, Deer, UserRecords
+from django.contrib.gis.geos import Point
+
 
 class CalculateChargeView(View):
 
@@ -12,8 +14,8 @@ class CalculateChargeView(View):
         data = json.loads(request.body)
 
         userdto = UserDTO()
-        user = User.objects.get(name= data["user_name"])
-        deer =  Deer.objects.get(name=data['deer_name'])
+        user = User.objects.get(name=data["user_name"])
+        deer = Deer.objects.get(name=data['deer_name'])
         userdto.user_id = user.id
         userdto.use_deer = deer
         userdto.use_end_lat = data['end_lat']
@@ -21,13 +23,14 @@ class CalculateChargeView(View):
         userdto.use_start_at = datetime.strptime(data['start_at'], '%Y-%m-%d %H:%M:%S')
         userdto.use_end_at = datetime.strptime(data['end_at'], '%Y-%m-%d %H:%M:%S')
 
-
         costCalculator = CostCalculator(userdto)
 
-        policies_and_charge =costCalculator.calculate()
+        policies_and_charge = costCalculator.calculate()
 
-        return JsonResponse({'message': 'SUCCESS', 'charge_cost':f'{policies_and_charge[-1]}, "요금 근거: f{policies_and_charge[:-1]}'}, status=200)
+        UserRecords.objects.create(user_id=user.id, deer_id=deer.id, start_at=userdto.use_start_at,
+                                   end_at=userdto.use_end_at,
+                                   end_lat_lng=Point(userdto.use_end_lat, userdto.use_end_lng))
 
-
-
-
+        return JsonResponse(
+            {'message': 'SUCCESS', 'charge_cost': f'{policies_and_charge[-1]}, "요금 근거: f{policies_and_charge[:-1]}'},
+            status=200)
